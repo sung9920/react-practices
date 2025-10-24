579a6327c93cc2a0263f872ba43fa53ad2edcbfc
sung9920 <sungcheol9920@gmail.com>
2025-10-24 17:49:56 +0900

    map() 사용해서 GroceryItem 보여주기

--- 03.Component-Property/ex01/src/GroceryList.js
+++ 03.Component-Property/ex01/src/GroceryList.js
@@ -7,18 +7,18 @@ function GroceryList({groceries}) {
     // const a = [<GroceryItem name={'bread'} count={10}/>, <GroceryItem name={'milk'} count={5}/>];
     
     /*
-    const a = groceries.map((item) => {
-        return <GroceryItem name={item.name} count={item.count} />
+    const a = groceries.map((item, index) => {
+        return <GroceryItem key={index} name={item.name} count={item.count} />
     });
     */
 
     return (
         <ol className={'grocery-list'}>
-            {groceries.map((item) => (
-                <GroceryItem name ={item.name} count={item.count} />
+            {groceries.map((item, index) => (
+                <GroceryItem key={index} name={item.name} count={item.count} />
             ))}
         </ol>
-    );
+    )
 }
 
 //React.createElement('ol', {}, <GroceryItem />, <GroceryItem />);
