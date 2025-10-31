50e89bc57469aaf3ce3cc0431db478ae050f7d8a
sung9920 <sungcheol9920@gmail.com>
2025-10-31 10:32:54 +0900

    tabbox #finish

--- 10.Integration/tabbox/frontend/src/TabBox.js
+++ 10.Integration/tabbox/frontend/src/TabBox.js
@@ -3,24 +3,20 @@ import data from "./assets/json/data.js";
 import { Tab_Box } from "./assets/scss/TabBox.scss";
 import TabView from "./TabView.js";
 import Tabs from "./Tabs.js";
-
 function TabBox() {
-  const [active, setActive] = useState(0);
-
+  const [active, setActive] = useState(data[0].no);
   const selectTab = (no) => {
-    setActive(data.findIndex(e=>e.no === no))
+    setActive(no);
   };
- 
+  const tabData = data.map((item) => ({
+    ...item,
+    active: item.no === active,
+  }));
   return (
     <div className={Tab_Box}>
-      <Tabs selectTab={selectTab} data={data.map((item, index) => ({
-        no: item.no,
-        name: item.name,
-        active: index === active
-      }))} />
-      <TabView contents={data[active].contents}/>
-    </div> 
+      <Tabs selectTab={selectTab} data={tabData} />
+      <TabView contents={data.find((item) => item.no === active).contents} />
+    </div>
   );
 }
-
 export default TabBox;
