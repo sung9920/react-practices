d80705b167c461d6fbd1c5b73ebc222251e6028b
sung9920 <sungcheol9920@gmail.com>
2025-10-31 17:36:13 +0900

    vite-tailwindcss

--- 11.Vite/vite-practices/src/App.tsx
+++ 11.Vite/vite-practices/src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="size-[100px] border-3 border-blue-700/90 bg-red-500/50 text-pink-500">
+      {/* 부모 div에 flex 추가, size는 w-auto 등으로 변경 */}
+      <div className="flex items-center gap-2 w-auto border-3 border-blue-700/90 bg-red-500/50 hover:bg-fuchsia-700/30 text-pink-500 p-4">
         Colors
       </div>
-      <button className="size-[100px] border-2 bg-amber-600 hover:bg-fuchsia-700 text-pink-700">ButtonA</button>
+      <button className="size-[100px] border-2 border-blue-700/90 bg-red-500 hover:bg-fuchsia-700 text-black">
+        ButtonA
+      </button>
     </>
   );
 }
-
 export default App;
